c787113c4af80cff74e0d7791621376d42b4f7cb
Mahmoud <mahmoud.hamdy12125@gmail.com>
2023-10-07 11:38:24 +0300

    NumberGame

---
+++ NumberGame/screens/StartGameScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput keyboardType="decimal-pad" />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    backgroundColor: "#72063c",
+    marginHorizontal: 20,
+    padding: 16,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+  },
+});
+
+export default StartGameScreen;
